faca27de7d595e85da6eb305d647adad6e0cabee
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2021-05-12 19:26:34 +0800

    feat: setup sanity studio preview

--- resolveProductionUrl.js
+++ resolveProductionUrl.js
@@ -1,3 +1,6 @@
+const projectUrl = process.env.SANITY_STUDIO_PROJECT_URL;
+const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET;
+
 export default function resolveProductionUrl(document) {
-  return `http://localhost:3000/movie/${document.slug.current}`;
+  return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`;
 }
